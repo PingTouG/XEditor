870d58233727d2ee8ae83f2b0018da9ff634d295
PingTouG <2798874773@qq.com>
2020-03-04 20:34:41 +0800

    chore: 添加NodeType

--- src/.internal/enums.ts
+++ src/.internal/enums.ts
@@ -5,3 +5,18 @@ export enum EClasses {
   SELECT = 'select',
   CONTENT = 'content',
 }
+
+export enum ENodeType {
+  ELEMENT = 1,
+  ATTRIBUTE,
+  TEXT,
+  CDATA_SECTION,
+  ENTITY_REFERENCE,
+  ENTITY,
+  PROCESSING_INSTRUCION,
+  COMMENT,
+  DOCUMENT,
+  DOCUMENT_TYPE,
+  DOCUMENT_FRAGMENT,
+  NOTATION,
+}
